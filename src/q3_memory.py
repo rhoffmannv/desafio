022ce70fc9957f0c5eee9c5f18d0708413d63bca
Rei <reinaldo.hoffmann@ug.uchile.cl>
2024-04-05 16:02:20 -0300

    Primer approach leyendo JSON linea por linea

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,27 @@
 from typing import List, Tuple
+import json
+from collections import Counter
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+    # Contador para usernames mencionados
+    counter_mentioned = Counter()
+    
+    with open(file_path, "r") as file:
+        # Leer linea a linea ie. tweet por tweet
+        for line in file:
+            # Leer tweet
+            tweet = json.loads(line)
+            # Extracción de "mentioned users"
+            mentioned = tweet.get('mentionedUsers')
+
+            if mentioned != None:
+                # Por cada user mencionado en el tweet
+                for user in mentioned:
+                    # Actualizar counter de menciones
+                    counter_mentioned[user['username']] += 1
+
+    # Extracción de los 10 usernames mas mencionados
+    out = counter_mentioned.most_common(10)
+
+    return out
